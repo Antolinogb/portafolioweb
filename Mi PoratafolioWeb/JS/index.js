c5786d266baa732db86281340d127be2547d59b8
Antolinogb <115845331+Antolinogb@users.noreply.github.com>
2023-06-07 20:22:42 -0100

    Add files via upload
    Este es mi portafolio

---
+++ Mi PoratafolioWeb/JS/index.js
@@ -0,0 +1,13 @@
+ /* const boton=document.getElementById("boton")
+ boton.addEventListener("click",mostrarNombre)
+ const h2=document.getElementById("h2")
+ const h1=document.getElementById("h1")
+ function mostrarNombre(){
+  const nombre=prompt("Introduce tu nombre")  
+  pasarNombre(nombre)
+ }
+ mostrarNombre()
+function pasarNombre(nombre){ 
+  h2.textContent="Hola"
+  h1.textContent=nombre
+}*/
