07224bc08ca39cad16dc7d2c3cf09038a31de975
Antolinogb <115845331+Antolinogb@users.noreply.github.com>
2023-07-13 17:03:00 -0100

    Add files via upload

---
+++ Mi PoratafolioWeb/JavascriptManipulacionDelDOM/color-aleatorio/index.js
@@ -0,0 +1,21 @@
+//sELECCIONAR LOS ELEMENTO DEL DOM
+const boton=document.querySelector("button");
+
+const color=document.getElementById("color");
+
+function generalColorAleatorio(){
+  let digitos='0123456789ABCDEF';
+  let colorHex='#';
+  for(let i=0;i<6;i++){
+let indiceAleatorio=Math.floor(Math.random()*16);
+colorHex +=digitos[indiceAleatorio];
+  }
+  return colorHex;
+}
+boton.addEventListener('click',function(){
+  let colorAleatorio=generalColorAleatorio();
+  //Actualizar el texto
+  color.textContent=colorAleatorio;
+  //Actualizar color de fondo
+  document.body.style.backgroundColor=colorAleatorio;
+})
